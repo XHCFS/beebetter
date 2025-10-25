0e0c320dd50f59bd995f4567dd9880370fb96ed1
salma-hatem <s-salma.hatem@zewailcity.edu.eg>
2025-10-25 13:42:52 +0200

    navigation bar v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:beebetter/TodayPage.dart';
+import 'package:beebetter/GuidedMode.dart';
+import 'package:beebetter/Dashboard.dart';
+import 'package:beebetter/ProfilePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,110 +17,150 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  final _pages = [
+    Dashboard(),
+    TodayPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: _pages[_selectedIndex],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        bottomNavigationBar: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+              boxShadow: [
+                BoxShadow(color: Theme.of(context).colorScheme.inversePrimary, spreadRadius: 0, blurRadius: 5),
+              ],
+            ),
+            child: Stack(
+                alignment: Alignment.bottomCenter,
+                clipBehavior: Clip.none,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0),
+                    ),
+                    child: BottomNavigationBar(
+                      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                      selectedItemColor: Theme.of(context).colorScheme.onPrimary,
+                      unselectedItemColor: Theme.of(context).colorScheme.primary,
+                      currentIndex: _selectedIndex,
+                      // showSelectedLabels: false,
+                      showUnselectedLabels: false,
+                      onTap: _onItemTapped,
+                      items:
+                        <BottomNavigationBarItem>[
+                          BottomNavigationBarItem(
+                            icon: _buildNavIcon(
+                              context,
+                              icon: Icons.today,
+                              isSelected: _selectedIndex == 0,
+                            ),
+                            label: 'Dashboard',
+                          ),
+                          BottomNavigationBarItem(
+                            icon: SizedBox.shrink(),
+                            label: ' ',
+                          ),
+                          BottomNavigationBarItem(
+                            icon: _buildNavIcon(
+                              context,
+                              icon: Icons.person,
+                              isSelected: _selectedIndex == 2,
+                            ),
+                            label: 'Profile',
+                          ),
+                      ],
+                    )
+                  ),
+
+                  Positioned(
+                    top: -15, // Moves circle up to overlap
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _selectedIndex = 1;
+                        });
+                      },
+                      child: Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 5,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Theme.of(context).colorScheme.inversePrimary,
+                              blurRadius: 15,
+                              offset: const Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: Icon(
+                          Icons.edit_note,
+                          color: Theme.of(context).colorScheme.primary,
+                          size: 50,
+                        ),
+                      ),
+                    ),
+                  ),
+              ]
+            )
+        )
     );
   }
+}
+
+Widget _buildNavIcon(BuildContext context,
+    {required IconData icon, required bool isSelected}) {
+  final colorScheme = Theme
+      .of(context)
+      .colorScheme;
+  return AnimatedContainer(
+    duration: const Duration(milliseconds: 200),
+    width: 45,
+    height: 45,
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: isSelected ? colorScheme.onPrimary : Colors.transparent,
+    ),
+    child: Icon(
+      icon,
+      color: isSelected ? colorScheme.inversePrimary : colorScheme.primary,
+      size: 28,
+    ),
+  );
 }
