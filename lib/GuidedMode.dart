2dc2f4859fdb3a32c419a9dcca05af731deace73
salma-hatem <s-salma.hatem@zewailcity.edu.eg>
2025-10-25 13:42:52 +0200

    navigation bar v1

---
+++ lib/GuidedMode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GuidedMode extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('GuidedMode', style: TextStyle(fontSize: 24))),
+    );
+  }
+}
---
+++ lib/TodayPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodayPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Today Page', style: TextStyle(fontSize: 24))),
+    );
+  }
+}
