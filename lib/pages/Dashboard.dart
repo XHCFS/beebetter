55dc24850fd06f01e98770c4ed02c24aabeae983
salma-hatem <s-salma.hatem@zewailcity.edu.eg>
2025-11-03 19:01:39 +0200

    Initial pages for Today and Guided Mode

--- lib/pages/Dashboard.dart
+++ lib/pages/Dashboard.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:beebetter/widgets/BackgroundGradient.dart';
 
 class Dashboard extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return BackgroundGradient(
       body: Center(child: Text('Dashboard', style: TextStyle(fontSize: 24))),
     );
   }
