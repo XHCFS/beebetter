fa79689c0e6079993cbe01707f435d14fe5b25e6
salma-hatem <s-salma.hatem@zewailcity.edu.eg>
2025-11-30 19:47:42 +0200

    Add stacking cards
    - saves user input
    - changes text field to read only when entry is submitted

--- lib/pages/GuidedMode/GuidedModeLogic.dart
+++ lib/pages/GuidedMode/GuidedModeLogic.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 
 class GuidedModeLogic extends ChangeNotifier {
   List<String> prompts = [
@@ -15,9 +16,13 @@ class GuidedModeLogic extends ChangeNotifier {
     "gratitude practice"
   ];
 
+  List<String> userInputs = [];
+  List<bool> canContinue = [];
+  List<bool> isDone = [];
+
+
   int currentPrompt = 0;
   int completedPrompts = 1;
-  bool canContinue = false;
 
   int get totalPrompts => prompts.length;
 
@@ -25,14 +30,27 @@ class GuidedModeLogic extends ChangeNotifier {
   String get currentPromptCategory => promptsCategory[currentPrompt];
 
 
+  GuidedModeLogic() {
+    final total = prompts.length;
+    userInputs = List.generate(total, (_) => "");
+    canContinue = List.generate(total, (_) => false);
+    isDone = List.generate(total, (_) => false);
+  }
 
   void updateCanContinue(bool value) {
+    if(isDone[currentPrompt]) return;
+
     if(completedPrompts <= totalPrompts) {
-      canContinue = value;
+      canContinue[currentPrompt] = value;
     }
     else{
-        canContinue = false;
+        canContinue[currentPrompt]  = false;
     }
+    notifyListeners();
+  }
+
+  void updatePromptInput(int index, String value) {
+    userInputs[index] = value;
     notifyListeners();
   }
 
@@ -41,35 +59,47 @@ class GuidedModeLogic extends ChangeNotifier {
     if(completedPrompts < totalPrompts) {
       completedPrompts++;
     }
-    print(entry);
-    canContinue = false;
+    canContinue[currentPrompt]  = false;
+    isDone[currentPrompt] = true;
     notifyListeners();
   }
 
   void nextPrompt() {
     if (currentPrompt < totalPrompts - 1) {
       currentPrompt++;
-      canContinue = false;
       notifyListeners();
     }
   }
 
-  void previousPrompt() {
+  void previousPrompt(CardSwiperController cardSwiperController) {
     if (currentPrompt > 0) {
-      currentPrompt--;
+      currentPrompt -- ;
+      cardSwiperController.moveTo(currentPrompt);
+      notifyListeners();
+    }
+  }
+
+  void onSwipe(int? currentIndex) {
+    if (currentIndex != null) {
+      currentPrompt = currentIndex;
       notifyListeners();
     }
   }
 
   void shufflePrompts() {
+    // TODO: we need to implement actual prompt shuffling logic here
     final combined = List.generate(prompts.length, (i) => {
       'prompt': prompts[i],
       'category': promptsCategory[i],
+      'input': userInputs[i],
     });
+
     combined.shuffle();
+
     for (int i = 0; i < combined.length; i++) {
       prompts[i] = combined[i]['prompt']!;
       promptsCategory[i] = combined[i]['category']!;
+      userInputs[i] = combined[i]['input']!;
     }
     notifyListeners();
   }
