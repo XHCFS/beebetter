0bf76984986a8cf1cdf551efd9209be2ed431a2d
CHEX <52354962+SFCHEX@users.noreply.github.com>
2025-12-26 22:08:00 +0200

    fix: restore main.dart to default state for CI/CD builds
    - Remove dependency on untracked voice_emotion_test_screen.dart
    - Use default MyHomePage to ensure builds succeed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'screens/voice_emotion_test_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Voice Emotion Test',
+      title: 'BEEbetter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const VoiceEmotionTestScreen(),
+      home: const MyHomePage(title: 'BEEbetter'),
     );
   }
 }
